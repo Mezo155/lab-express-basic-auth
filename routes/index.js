c7ae008a71336790192a6dd4404edf41c7f1de66
Mezo155 <tonicb155@hotmail.com>
2024-04-13 00:59:39 +0200

    1ª parte

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,14 @@ const router = require("express").Router();
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+router.post("/", (req, res, next) => {
+  User.create(req.body)
+  .then((user) => {
+    res.redirect("/");
+  })
+  .catch((err) => {
+res.render("/", {errors: err.erros, user: req.body});
+  })
+})
 
 module.exports = router;
